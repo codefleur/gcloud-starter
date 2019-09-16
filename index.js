a00e0f1928912df2e25a1a33717580b927827048
choephixpi <choephix.pi@gmail.com>
2019-09-16 19:19:12 +0000

    index

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const PORT = process.env.PORT || 1337
-const URL_PING = process.env.URL_PING || "http://gccxpi.duckdns.org/"
+const PORT = process.env.PORT || 80
+const URL_PING = process.env.URL_PING || "http://gccxpi.duckdns.org/api/config"
 const URL_REDIRECT = process.env.URL_REDIRECT || "http://gccxpi.duckdns.org/"
 const COMPUTE_ZONE = process.env.COMPUTE_ZONE
 const COMPUTE_INSTANCE = process.env.COMPUTE_INSTANCE
+const SPINNER = process.env.SPINNER || "purple"
 
 const compute = new (require('@google-cloud/compute'))()
 const fetch = require("node-fetch")
@@ -17,63 +18,23 @@ async function startInstance() {
   const [error, operation, response ] = await zone.vm(COMPUTE_INSTANCE).start()
   const something = await operation
   console.log(something)
-  
   await sleep(1000)
-
-  // while( true )
-  // {
-  //   await sleep(100)
-  //   try { 
-  //     let response = await fetch(URL_PING,{timeout:900})
-  //     let text = await response.text()
-  //     console.log( `> Response received: ${text}` )
-  //     break
-  //   }
-  //   catch( e ) { console.error(e.message) }
-  // }
-
   console.log( "> Instance started!" )
 }
 
-// const WebSocketServer = require('websocket').server;
-// const https = require('https')
 const http = require('http')
 const express = require('express')
 const app = express()
+const CONFIG_JS = `window.URL_PING="${URL_PING}"\nwindow.URL_REDIRECT="${URL_REDIRECT}"\nwindow.SPINNER="${SPINNER}"`
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 app.use('/',express.static('www'))
-app.use('/start',(req,res)=>{
-  startInstance()
-  let html = `
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script>
-    function ping() { $.ajax({ url:"${URL_PING}", error:console.warn, success: onReady, timeout: 1000 }) }
-    function onReady() { window.location.href="${URL_REDIRECT}" }
-    ping()
-    setInterval( ping, 2000 )
-  </script>`
-  res.send(html)
-})
+app.use('/config.js', (req,res) => res.send( CONFIG_JS ) )
+app.use('/vm/start', (req,res) => {  } )
 
 const server = http.createServer( app )
 server.listen( PORT, () => console.log(`Serving on ${server.address().port}`) )
-
-// let wsServer = new WebSocketServer({ httpServer: server })
-
-// wsServer.on('request', function(request) {
-//   var connection = request.accept(null, request.origin);
-//   connection.on('message', function(message) {
-//     if (message.type === 'utf8') {
-//       // process WebSocket message
-//     }
-//   });
-//   connection.on('close', function(connection) {
-//     // close user connection
-//   });
-// } )
